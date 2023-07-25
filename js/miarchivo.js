e6e3f0b6beba260989e7f8712bb251c461a3ec25
Christian Kosak <christiankosak@hotmail.com>
2023-07-25 17:02:08 +0200

    Tercera Pre-entrega

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -1,7 +1,7 @@
 // Declaración de array para almacenar los productos
 let productos = [];
 
-// Declaración de función para agregar un producto al array
+// Función para agregar un producto al array
 function agregarProducto(nombre, costo) {
   const producto = {
     nombre: nombre,
@@ -10,14 +10,20 @@ function agregarProducto(nombre, costo) {
   productos.push(producto);
 }
 
-// Declaración de función para buscar un producto por nombre
+// Función para buscar un producto por nombre
 function buscarProducto(nombre) {
   for (let i = 0; i < productos.length; i++) {
     if (productos[i].nombre === nombre) {
       return productos[i];
     }
   }
-  return null; // Retorna null si el producto no se encuentra
+  return null; 
+}
+
+// Función para mostrar el resultado en el DOM
+function mostrarResultado(resultado) {
+  const resultadoDiv = document.getElementById("resultado");
+  resultadoDiv.innerHTML = resultado;
 }
 
 // Algoritmo principal
@@ -31,7 +37,7 @@ function simuladorCostos() {
 
     if (!isNaN(costo)) {
       costoTotal += costo;
-      agregarProducto(nombre, costo); // Agrega el producto al array
+      agregarProducto(nombre, costo); 
     } else {
       alert("Costo inválido. Intente nuevamente.");
       continue;
@@ -44,19 +50,33 @@ function simuladorCostos() {
     }
   }
 
-  alert("El costo total de los productos seleccionados es: " + costoTotal.toFixed(2));
+  // Mostrar el costo total de los productos seleccionados
+  let resultado = "El costo total de los productos seleccionados es: $" + costoTotal.toFixed(2);
+  mostrarResultado(resultado);
 
   let buscarProductoNombre = prompt("Ingrese el nombre del producto a buscar:");
-  let productoEncontrado = buscarProducto(buscarProductoNombre); // Busca el producto en el array
+  let productoEncontrado = buscarProducto(buscarProductoNombre); 
 
   if (productoEncontrado) {
-    alert("El producto \"" + productoEncontrado.nombre + "\" fue encontrado. Su costo es: " + productoEncontrado.costo.toFixed(2));
+    resultado = "El producto \"" + productoEncontrado.nombre + "\" fue encontrado. Su costo es: $" + productoEncontrado.costo.toFixed(2);
   } else {
-    alert("El producto \"" + buscarProductoNombre + "\" no fue encontrado.");
+    resultado = "El producto \"" + buscarProductoNombre + "\" no fue encontrado.";
   }
+  mostrarResultado(resultado);
+
+  // Guardar productos en el localStorage usando JSON
+  localStorage.setItem("productos", JSON.stringify(productos));
 }
 
-// Agregar los productos
+// Cargar productos almacenados en el localStorage al iniciar la página
+document.addEventListener("DOMContentLoaded", function () {
+  const storedProductos = localStorage.getItem("productos");
+  if (storedProductos) {
+    productos = JSON.parse(storedProductos);
+  }
+});
+
+
 agregarProducto("Remera", 10000);
 agregarProducto("Pantalón", 20000);
 agregarProducto("Malla", 15000);
